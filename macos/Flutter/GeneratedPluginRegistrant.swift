9eb294e40bee4c7bc3a7217bcde78a82abee8b12
Martifu <martin0013esp@hotmail.com>
2023-08-24 15:40:49 -0600

    cambios

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import elevenlabs
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ElevenlabsPlugin.register(with: registry.registrar(forPlugin: "ElevenlabsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
